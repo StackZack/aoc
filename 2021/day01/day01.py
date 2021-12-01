98dff28a6109231ee94e1c5ae462780354a1a27c
Zackary Luedtke <luedtkezack@gmail.com>
2021-12-01 08:25:46 -0600

    Completed second solution of day01.

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,16 +1,40 @@
 from csv import reader
+from queue import Queue
 import os
 
 
-def get_count_of_depth_increases(depthList):
+def count_depth_increases_by_window(depthList):
+    count = 0
+    prevWindow = None
+    q = Queue(maxsize=3)
+    for index, depth in enumerate(depthList, start=0):
+        if len(depthList) - index == 2:
+            break
+        if prevWindow is None:
+            prevWindow = depth
+            q.put(depth)
+            continue
+
+        if len(list(q.queue)) == 3:
+            q.get()
+        q.put(depth)
+        windowSum = sum(list(q.queue))
+
+        if windowSum > prevWindow:
+            count += 1
+        prevWindow = windowSum
+    return count
+
+
+def count_depth_increases(depthList):
     prevDepth = None
     count = 0
     for depth in depthList:
         if prevDepth is None:
             prevDepth = depth
-            pass
+            continue
         if depth > prevDepth:
-            count = count + 1
+            count += 1
         prevDepth = depth
     return count
 
@@ -31,5 +55,10 @@ def read_depth_file(filePath):
 
 if __name__ == "__main__":
     depthList = read_depth_file("input.csv")
-    numOfIncreases = get_count_of_depth_increases(depthList)
+    numOfIncreases = count_depth_increases(depthList)
     print(f"There were {numOfIncreases} increases in depth.")
+    numOfIncreasesByWindow = count_depth_increases_by_window(depthList)
+    print(
+        "There were {} increases in depth in the sliding window."
+        .format(numOfIncreasesByWindow)
+    )
