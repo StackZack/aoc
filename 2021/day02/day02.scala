6624a66688f16d956948db524a5af0dd44912f70
Zackary Luedtke <luedtkezack@gmail.com>
2021-12-02 19:17:38 -0600

    Refactored day02 Scala solution for readability and to be more functional.

--- 2021/day02/day02.scala
+++ 2021/day02/day02.scala
@@ -4,30 +4,27 @@ object Main extends App {
   val inputFile: BufferedSource = io.Source.fromFile("input.csv")
   val commandList: List[String] = (for (line <- inputFile.getLines()) yield line).toList
 
-  val (subX, subY) = processCommands(commandList)
-  println("Horizontal and vertical product: %d" format subX * subY)
-  val (subWithAimX, subWithAimY, _) = processCommandsWithAim(commandList)
-  println("Horizontal and vertical product with aim: %d" format subWithAimX * subWithAimY)
+  val (posX, posY) = sumCommands(commandList)
+  println("Horizontal and vertical product: %d" format posX * posY)
+  val (posWithAimX, posWithAimY, _) = sumCommandsWithAim(commandList)
+  println("Horizontal and vertical product with aim: %d" format posWithAimX * posWithAimY)
 
-  def processCommands(commandList: List[String]): (Int, Int) = {
-    var pos = (0, 0)
-    for(command <- commandList) {
-      val splitCommand = (command.split(" ")(0), command.split(" ")(1).toInt)
-      pos = movePos(pos, splitCommand)
-    }
-    return pos
+  def sumCommands(commandList: List[String]): (Int, Int) = {
+    val commandListSplit = commandList.map(command => (command.split(" ")(0), command.split(" ")(1).toInt))
+    val positions = commandListSplit.map(command => movePos((command._1, command._2)))
+    return positions.foldLeft(0, 0)((sumPos, pos) => (sumPos._1 + pos._1, sumPos._2 + pos._2))
   }
 
-  def movePos(pos: (Int, Int), command: (String, Int)): (Int, Int) = {
-    val (x, y) = pos
-    command._1 match {
-      case "up" => (x, y - command._2)
-      case "down" => (x, y + command._2)
-      case "forward" => (x + command._2, y)
+  def movePos(command: (String, Int)): (Int, Int) = {
+    val (direction, distance) = command
+    (direction, distance) match {
+      case ("up", _) => (0, 0 - distance)
+      case ("down", _) => (0, 0 + distance)
+      case ("forward", _) => (0 + distance, 0)
     }
   }
 
-  def processCommandsWithAim(commandList: List[String]): (Int, Int, Int) = {
+  def sumCommandsWithAim(commandList: List[String]): (Int, Int, Int) = {
     var pos = (0, 0, 0)
     for(command <- commandList) {
       val splitCommand = (command.split(" ")(0), command.split(" ")(1).toInt)
@@ -38,10 +35,11 @@ object Main extends App {
 
   def movePosWithAim(pos: (Int, Int, Int), command: (String, Int)): (Int, Int, Int) = {
     val (x, y, z) = pos
-    command._1 match {
-      case "up" => (x, y, z - command._2)
-      case "down" => (x, y, z + command._2)
-      case "forward" => (x + command._2, y + (z * command._2), z)
+    val (direction, distance) = command
+    (direction, distance) match {
+      case ("up", _) => (x, y, z - distance)
+      case ("down", _) => (x, y, z + distance)
+      case ("forward", _) => (x + distance, y + (z * distance), z)
     }
   }
 }
