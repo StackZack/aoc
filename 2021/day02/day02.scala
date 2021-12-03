5cb408cef04fa0ced61284665d80be86629ee59a
Zackary Luedtke <luedtkezack@gmail.com>
2021-12-02 18:42:24 -0600

    Completed day02 part2 in Scala.

--- 2021/day02/day02.scala
+++ 2021/day02/day02.scala
@@ -4,8 +4,10 @@ object Main extends App {
   val inputFile: BufferedSource = io.Source.fromFile("input.csv")
   val commandList: List[String] = (for (line <- inputFile.getLines()) yield line).toList
 
-  val submarinePos = processCommands(commandList)
-  println("Horizontal and vertical product: %d" format submarinePos._1 * submarinePos._2)
+  val (subX, subY) = processCommands(commandList)
+  println("Horizontal and vertical product: %d" format subX * subY)
+  val (subWithAimX, subWithAimY, _) = processCommandsWithAim(commandList)
+  println("Horizontal and vertical product with aim: %d" format subWithAimX * subWithAimY)
 
   def processCommands(commandList: List[String]): (Int, Int) = {
     var pos = (0, 0)
@@ -17,10 +19,29 @@ object Main extends App {
   }
 
   def movePos(pos: (Int, Int), command: (String, Int)): (Int, Int) = {
+    val (x, y) = pos
     command._1 match {
-      case "up" => (pos._1, pos._2 - command._2)
-      case "down" => (pos._1, pos._2 + command._2)
-      case "forward" => (pos._1 + command._2, pos._2)
+      case "up" => (x, y - command._2)
+      case "down" => (x, y + command._2)
+      case "forward" => (x + command._2, y)
+    }
+  }
+
+  def processCommandsWithAim(commandList: List[String]): (Int, Int, Int) = {
+    var pos = (0, 0, 0)
+    for(command <- commandList) {
+      val splitCommand = (command.split(" ")(0), command.split(" ")(1).toInt)
+      pos = movePosWithAim(pos, splitCommand)
+    }
+    return pos
+  }
+
+  def movePosWithAim(pos: (Int, Int, Int), command: (String, Int)): (Int, Int, Int) = {
+    val (x, y, z) = pos
+    command._1 match {
+      case "up" => (x, y, z - command._2)
+      case "down" => (x, y, z + command._2)
+      case "forward" => (x + command._2, y + (z * command._2), z)
     }
   }
 }
